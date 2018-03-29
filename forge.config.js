c82d3218057bb72960a73ab41768a971cc66a9ca
eliot-akira <me@eliotakira.com>
2018-03-29 07:40:58 +0200

    Use fork of electron-compile and electron-compilers to prevent compiling Sass partials

--- forge.config.js
+++ forge.config.js
@@ -1,8 +1,12 @@
+const path = require('path')
+
 // https://github.com/electron-userland/electron-forge/#config
+
 module.exports = {
   make_targets: {
     darwin: [
       'zip',
+      //
     ],
     /*linux: [
       'deb',
@@ -12,19 +16,38 @@ module.exports = {
       'squirrel',
     ],*/
   },
+
   // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md
+
   electronPackagerConfig: {
     //packageManager: 'yarn',
     icon: './icons/icon',
-    derefSymlinks: false,
-    ignore: [
+    derefSymlinks: true,
+    ignore:
+    // TODO: Make this a function and ignore all unnecessary stuff like devDependencies
+    [
       // RegEx to match against absolute path of file/directory
-      /\/naudio\/third_party/,
-      /\/forge\//,
+      /\/naudio\/third_party\//,
+      /\.hardlinks$/,
     ],
+
+    // Disable compiling Sass partials
+    // https://github.com/electron-userland/electron-compilers/pull/81
+    // https://github.com/electron-userland/electron-compile/pull/260
+
+    afterPrune: [(buildPath, electronVersion, platform, arch, callback) => {
+
+      const { execSync } = require('child_process')
+      execSync('npm install github:eliot-akira/electron-compile#built', {
+        cwd: path.join(buildPath, 'node_modules')
+      })
+
+      callback()
+    }]
   },
 
   // https://github.com/electron/electron-rebuild
+/*
 
   electronInstallerDebian: {},
   electronInstallerRedhat: {},
@@ -32,7 +55,7 @@ module.exports = {
     owner: '',
     name: '',
   },
-  /*electronWinstallerConfig: {
+  electronWinstallerConfig: {
     name: 'audiom',
   },
   windowsStoreConfig: {
